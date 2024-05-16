f3bf6d411aae663fd7e6eda338f8ce173a1a99e0
Bobby <143777624+306bobby-android@users.noreply.github.com>
2024-05-16 12:45:33 -0400

    Update setup.sh
    *Correct order of operations

--- setup.sh
+++ setup.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env sh
-
-	_file="$(find $PREFIX/lib/python3.11 -name "_sysconfigdata*.py")"
-rm -rf $PREFIX/lib/python3.11/__pycache__
-cp $_file "$_file".backup
-sed -i 's|-fno-openmp-implicit-rpath||g' "$_file"
-
 	pkg update -qy
-	pkg install -qy --show-progress coreutils build-essential gawk sharutils p7zip \
+	pkg install -qy --show-progress curl grep wget coreutils build-essential gawk sharutils p7zip \
 		gzip cpio bzip2 liblz4 liblzma xz-utils lzop \
 		libxml2 python aria2 brotli zstd openssl
 
+	_file="$(find $PREFIX/lib/python3.11 -name "_sysconfigdata*.py")"
+	rm -rf $PREFIX/lib/python3.11/__pycache__
+	cp $_file "$_file".backup
+	sed -i 's|-fno-openmp-implicit-rpath||g' "$_file"
 
 if [ $? -eq 0 ]; then
 	wget -q -O get-pip.py https://bootstrap.pypa.io/get-pip.py
